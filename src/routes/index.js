21e93f4904c5b67ef30bb1090e4a510d57159c0c
mateuskuritza <mateuskuritza@gmail.com>
2022-02-11 10:32:39 -0300

    Preparando revisão de código

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authRouter from "./authRouter.js";
-import cashRouter from "./cashRouter.js";
-
-const router = Router();
-router.use(authRouter);
-router.use(cashRouter);
-export default router;
--- src/schemas/cashSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi'
-
-const cashSchema = joi.object({
-    value : joi.number().required().greater(0),
-    description : joi.string().required(),
-})
-
-export default cashSchema;
